0520631d0a4371d7f5e2fe0e9fa865f4266a1d59
XavierRLX <renanlima2000.aer@gmail.com>
2024-07-26 11:56:40 -0300

    feat
    add .css

--- public/javaScript/resultado.js
+++ public/javaScript/resultado.js
@@ -1,7 +1,7 @@
 const supabaseUrl = 'https://pzjmkmgqjxvmhmhuddiy.supabase.co';
 
 async function carregaVotos() {
-    const url = `${supabaseUrl}/rest/v1/users?ativo=eq.true`;
+    const url = `${supabaseUrl}/rest/v1/users?ativo=eq.true&order=totalVotos.desc`;
     const response = await fetch(url, {
         method: 'GET',
         headers: {
