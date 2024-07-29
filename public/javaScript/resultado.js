b328a73fd290c9814c0b5960952a3aae30bc60be
XavierRLX <renanlima2000.aer@gmail.com>
2024-07-29 10:06:56 -0300

    feat
    style resultado

--- public/javaScript/resultado.js
+++ public/javaScript/resultado.js
@@ -1,30 +1,35 @@
 const supabaseUrl = 'https://pzjmkmgqjxvmhmhuddiy.supabase.co';
 
-async function carregaVotos() {
-    const url = `${supabaseUrl}/rest/v1/users?ativo=eq.true&order=totalVotos.desc`;
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6am1rbWdxanh2bWhtaHVkZGl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEyNDMzMjAsImV4cCI6MjAzNjgxOTMyMH0.SgizVK5qT6ibYWQ4GRO1Ppy9cuaNTvchy-bsWT-0YYY'
-        },
-    });
+        async function carregaVotos() {
+            const url = `${supabaseUrl}/rest/v1/users?ativo=eq.true&order=totalVotos.desc`;
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6am1rbWdxanh2bWhtaHVkZGl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEyNDMzMjAsImV4cCI6MjAzNjgxOTMyMH0.SgizVK5qT6ibYWQ4GRO1Ppy9cuaNTvchy-bsWT-0YYY'
+                },
+            });
 
-    if (response.ok) {
-        const data = await response.json();
-        const totalList = document.getElementById('totalList');
-        totalList.innerHTML = '';
-        data.forEach((user) => {
-            const listItem = document.createElement('div');
-            listItem.innerHTML = `
-                <p>${user.nameuser}: ${user.totalVotos} votos</p>
-            `;
-            totalList.appendChild(listItem);
-        });
-    } else {
-        alert('Erro ao carregar a votação');
-    }
-}
+            if (response.ok) {
+                const data = await response.json();
+                const totalList = document.getElementById('totalList');
+                totalList.innerHTML = '';
+                data.forEach((user, index) => {
+                    const listItem = document.createElement('div');
+                    listItem.classList.add('list-item');
+                    listItem.innerHTML = `
+                        <div class="ranking">${index + 1}º - </div>
+                        <p>${user.nameuser}: ${user.totalVotos} votos</p>
+                    `;
+                    totalList.appendChild(listItem);
+                });
+            } else {
+                alert('Erro ao carregar a votação');
+            }
+        }
+
+        // Chama a função para carregar os votos quando a página for carregada
+        carregaVotos();
 
 // Chamar a função para carregar os votos quando a página carregar
 document.addEventListener('DOMContentLoaded', carregaVotos);
